76556e2ff7dede1f244acf070de094ad220c3f4b
Simon Gerhard <simon.gerhard@outlook.com>
2023-02-01 16:16:57 +0100

    aufgabe 1.4

---
+++ Uebungen/Uebung1.4/calc.php
@@ -0,0 +1,30 @@
+<?php
+function calc()
+{
+    $number1 = intval($_GET['number1']);
+    $number2 = intval($_GET['number2']);
+    $mode = $_GET['mode'];
+
+    if($mode == "plus")
+    {
+        return $number1 + $number2;
+    }
+
+    if($mode == "minus")
+    {
+        return $number1 - $number2;
+    }
+
+    if($mode == "mal")
+    {
+        return $number1 * $number2;
+    }
+
+    if($mode == "div")
+    {
+        return $number1 / $number2;
+    }
+
+}
+    echo calc();
+?>
